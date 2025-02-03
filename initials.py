a5d37b1946c318deb93685e04cc99801da3809c1
Siddharth <chyani341@gmail.com>
2025-02-03 01:12:47 -0700

    My Version

--- initials.py
+++ initials.py
@@ -1,13 +1,13 @@
-def getInitials(full_name):
-   name_parts = full_name.split()
-   initials = [part[0].upper() + '. ' for part in name_parts]
+def get_initials(name):
+  name_list = name.split(" ")
+  initials = [part[0].upper() + '. ' for part in name_list]
 
-   return ''.join(initials)
+  return "".join(initials)
+
 def main():
-   name = input("Enter full name: ").strip()
+   full_name = input("Enter your full name: ")
 
-   print(f"Intials: ", getInitials(name))
+   print(f"Initials for {full_name} are: ", get_initials(full_name))
+   
+
 main()
-
-
-     
--- quiz.py
+++ quiz.py
@@ -4,26 +4,23 @@ randNum = random.randint(0,50)
 guesses = 0
 
 while True:
-    guess = input("Enter a number between 0 and 50: ")
-    if guess.isdigit():
-        guess = int(guess)
-        if not( 0 < guess < 50):
-            print("Out of bounds")
+    guesses += 1
+    userGuess = input("Enter a number between 0 and 50: ")
+    if userGuess.isdigit():
+        userGuess = int(userGuess)
+        if not(0 <= userGuess <= 50):
+            print("Out of bounds.")
             continue
     else:
-        print("Not a number")
+        print("Not a digit.")
         continue
 
-
-    guesses += 1
-
-    if guess == randNum:
-        print("Youre right")
+    if userGuess == randNum:
+        print("You're right!")
         break
-    elif guess < randNum:
-        print("Guess higher")
-        continue
+    elif userGuess <= randNum:
+        print("Guess higher.")
     else:
-        print("Guess lower")
-    
-print(f"Guesses: " , guesses)
+        print("Guess lower.")
+
+print("Total guesses:", guesses)
