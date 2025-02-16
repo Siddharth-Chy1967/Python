535fca9e8fbb8c306933959a4e45f77fe0c2c00c
Siddharth Chowdhury <chyani341@gmail.com>
2025-02-16 16:55:09 -0700

    Practice

--- binary_search.py
+++ binary_search.py
@@ -13,23 +13,7 @@ def binary_search(arr, target):
 
     return -1
 
-def main():
-    size = int(input("Enter the size: "))
-    print(f"Enter {size} ordered elements separated by space: ")
-    arr = list(map(int, input().split()))
 
-    if len(arr) != size:
-        print(f"Expected {size} but got {len(arr)}")
-    target = int(input("Enter the target to search: "))
-
-    result = binary_search(arr, target)
-
-    if result != -1:
-
-        print(f"Target is at index ", result)
-
-    else:
-        print("Not found")
 
 if __name__ == "__main__":
     main()
