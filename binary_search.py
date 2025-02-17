d497bc249889a0bcdd423d31f547fc78dee09e95
Siddharth Chowdhury <chyani341@gmail.com>
2025-02-16 17:18:53 -0700

    almost completed binary search

--- binary_search.py
+++ binary_search.py
@@ -3,17 +3,31 @@ def binary_search(arr, target):
     right = len(arr) - 1
 
     while(left <= right):
-        mid = (left + right)//2
-        if arr[mid] == target:
+        mid = (left + right)/2
+        if(arr[mid] == target):
             return mid
-        elif arr[mid] < target:
+        elif(arr[mid] < target):
             left = mid + 1
-        elif arr[mid] > target:
+        elif(arr[mid] > target):
             right = mid - 1
+        
 
     return -1
 
 
+def main():
+    arr = list(input("Enter a list of numbers to search from: "))
+    if all (parts in arr.isdigit() for parts in arr):
+        arr.split(" ")
+        arr = int(arr)
+    else:
+        print("Atleast one input was not a number.")
 
+    target = input("Enter target to search: ")
+
+    index = binary_search(arr, target)
+
+    if not (index == -1):
+        print(f"The target is in {}")
 if __name__ == "__main__":
     main()
